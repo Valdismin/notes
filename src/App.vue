b131eb943fd3bb81c9865c8ad2261d702db0268c
vladislav.kozlovski <vkozlovsky@atypicaldigital.com>
2022-12-15 18:32:23 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <div id="app">
+        <main-component/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainComponent from './components/MainComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,83 @@
 <template>
-$END$
+  <div>
+    <h1>{{title}}</h1>
+    <div class="new-note">
+      <input type="text" v-model="newNote.title">
+      <input type="text" v-model="newNote.description">
+      <button @click="addNote">Add Note</button>
+      <div v-show="message">
+        {{message}}
+      </div>
+    </div>
+    <div class="notes">
+      <div class="note" v-for="(note, index) in notes" :key="index">
+        <div class="node-header">
+          <p>{{note.title}}</p>
+        </div>
+        <div class="note-body">
+          <p>{{note.description}}</p>
+          <span>{{note.date}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "MainComponent"
+  name: "MainComponent",
+  data() {
+    return {
+      title: "Notes",
+      notes: [
+        {
+          title: "Note 1",
+          description: "Description for 1 note",
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          title: "Note 2",
+          description: "Description for 2 note",
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          title: "Note 3",
+          description: "Description for 3 note",
+          date: new Date(Date.now()).toLocaleString()
+        },
+      ],
+      newNote: {
+        title:"",
+        description:""
+      },
+      message: ""
+    }
+  },
+  methods: {
+    addNote () {
+      let {title, description} = this.newNote
+
+      if (title === "") {
+        this.message = "title can't be blank"
+        return false
+      }
+
+      if (description === "") {
+        this.message = "description can't be blank"
+        return false
+      }
+
+      this.notes.push({
+        title,
+        description,
+        date: new Date(Date.now()).toLocaleString()
+      })
+
+      this.newNote.title = ""
+      this.newNote.description = ""
+      this.message = ""
+    }
+  }
 }
 </script>
 
